4cf143c7d662c1b8149b0f02c6c701ae4d07695e
wenzm <3334243531@qq.com>
2026-09-21 15:25:07 +0800

    同步 v0.3.4(versionCode 30)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,8 +9,8 @@ android {
         applicationId = "top.hnwen17.guard"
         minSdk = 30
         targetSdk = 35
-        versionCode = 29
-        versionName = "0.3.3"
+        versionCode = 30
+        versionName = "0.3.4"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     flavorDimensions += "data"
